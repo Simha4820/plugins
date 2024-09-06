bffcff52467cecf605a2121d3bdb5c72a16e2c49
Sai Simha Reddy <109609468+Simha4820@users.noreply.github.com>
2024-09-06 10:28:50 +0530

    Create nagios_script.sh
    Nagios Commit

---
+++ nagios_script.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+yum install nagios-plugins-all nagios-plugins-nrpe nrpe
+cp /etc/nagios/nrpe.cfg /etc/nagios/nrpe.cfg.bkp
+cp nrpe.cfg /etc/nagios/nrpe
+cp nagios/* /usr/lib64/nagios/plugins/
+chmod 0755 check_*
+systemctl restart nrpe
+systemctl status nrpe
+
+/usr/lib64/nagios/plugins/check_users -w 3 -c 5
+/usr/lib64/nagios/plugins/check_load -w 15,10,5 -c 30,25,20
+/usr/lib64/nagios/plugins/check_mem -w 80 -c 90
+/usr/lib64/nagios/plugins/check_disk -w 20% -c 10% -p /dev/nvme0n1p1
+/usr/lib64/nagios/plugins/check_procs -w 350 -c 450
+/usr/lib64/nagios/plugins/check_swap -w 20% -c 10%
+/usr/lib64/nagios/plugins/check_uptime
